4615ae743a3b26ab9a8b769d46c2b67212037f24
Vladimir Romanov <vladromweb@gmail.com>
2016-10-10 20:34:30 -0700

    Added UIPageViewController Tutorial
    Added UIPageViewController Tutorial

--- MultithreadingTutorial/MultithreadingTutorial/ViewController.swift
+++ MultithreadingTutorial/MultithreadingTutorial/ViewController.swift
@@ -53,7 +53,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.activityIndicator.stopAnimating()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
---
+++ PageViewControllerTutorial/PageViewControllerTutorial/PageVC.swift
@@ -0,0 +1,93 @@
+//
+//  PageVC.swift
+//  PageViewControllerTutorial
+//
+//  Created by Volodymyr Romanov on 10/10/16.
+//  Copyright © 2016 Vladimir Romanov. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    lazy var VCArr: [UIViewController] = {
+        return [self.VCInstance(name: "FirstVC"),
+                self.VCInstance(name: "SecondVC"),
+                self.VCInstance(name: "ThirdVC")]
+    }()
+    
+    private func VCInstance(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+        if let firstVC = VCArr.first {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        for view in self.view.subviews {
+            if view is UIScrollView {
+                view.frame = UIScreen.main.bounds
+            } else if view is UIPageControl {
+                view.backgroundColor = UIColor.clear
+            }
+        }
+    }
+    
+
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = VCArr.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return VCArr.last
+        }
+        
+        guard VCArr.count > previousIndex else {
+            return nil
+        }
+        
+        return VCArr[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = VCArr.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < VCArr.count else {
+            return VCArr.first
+        }
+        
+        guard VCArr.count > nextIndex else {
+            return nil
+        }
+        
+        return VCArr[nextIndex]
+    }
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return VCArr.count
+    }
+    
+
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = VCArr.index(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+}
